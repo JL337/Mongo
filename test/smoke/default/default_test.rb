aaf0d67903364420f128693bc4f463b9d5b7c783
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-02-06 11:29:42 +0000

    Mongo 3.2 installed, running, enabled, listening on 27017, address 0.0.0.0

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,25 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# Check Mongodb has been installed
+#mogod version test
+describe package('mongodb-org') do
+	it {should be_installed}
+	its ('version') {should match /3\.2\../}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# Check Mongodb is running and enabled
+describe service('mongod') do
+	it {should be_running}
+	it {should be_enabled}
 end
+
+describe port(27017) do
+  it { should be_listening }
+  its('addresses') {should include '0.0.0.0'}
+end
+
+
+
+
+
